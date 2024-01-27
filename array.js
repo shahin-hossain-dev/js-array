b3c67c30cae5ac4ca4f2e83976ea1852c6724139
shahin-hossain-dev <shahin.hossain.dev@gmail.com>
2024-01-28 00:19:44 +0600

    17.5-1 Store More Than One Value In A Variable Using Array

---
+++ array.js
@@ -0,0 +1,14 @@
+/**
+ * ****************
+ *      ARRAY
+ * ****************
+ * array declare করতে ৩টা জিনিস লাগে ।
+ * 
+ * 1. square Bracket = []
+ * 2. এক বা একাধিক ভ্যালু থাকবে = 10
+ * ৩. এবং প্রতি element মধ্যে একটা কমা বসবে = ,
+ * ৪. ‍প্রতিটা element এর পরে একটা space দেয়া।
+ */
+
+// array এর মধ্যে same type এর মান গুলো রাখতে হবে। various value preferable না ।
+const numbers = [10, 20, 30, 40, 50];
