e385f7e2b9fc023c2390b4da5e663c788b3b828f
shahin-hossain-dev <shahin.hossain.dev@gmail.com>
2024-01-28 00:42:04 +0600

    17-2 Array Length, Index, Get And Set By Index

---
+++ index.js
@@ -0,0 +1,18 @@
+// ---------Index------------
+
+const numbers = [10, 20, 30, 40, 50, 60];
+
+// when find index give index number in square bracket []
+
+// get element value by index
+
+/* console.log(numbers[0]);
+console.log(numbers[3]);
+const fifth = numbers[5];
+console.log(fifth) */
+
+// set element value by index
+
+numbers[1] = 100;
+
+console.log(numbers)
---
+++ length.js
@@ -0,0 +1,4 @@
+// ------------array length------------
+
+const numbers = [10, 20, 30, 40, 50, 60];
+console.log(numbers.length)
